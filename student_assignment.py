0073725959dc271b5de07dd293564d9d8eef9163
Shirley1-Huang <Shirley1_Huang@asus.com>
2025-03-07 17:13:34 +0800

    Update student_assignment.py

--- student_assignment.py
+++ student_assignment.py
@@ -12,7 +12,23 @@ gpt_emb_config = get_model_configuration(gpt_emb_version)
 dbpath = "./"
 
 def generate_hw01():
-    pass
+    chroma_client = chromadb.PersistentClient(path = dbpath)
+    
+    openai_ef = embedding_functions.OpenAIEmbeddingFunction(
+        api_key = gpt_emb_config['api_key'],
+        api_base = gpt_emb_config['api_base'],
+        api_type = gpt_emb_config['openai_type'],
+        api_version = gpt_emb_config['api_version'],
+        deployment_id = gpt_emb_config['deployment_name']
+    )
+    
+    collection = chroma_client.get_or_create_collection(
+        name="TRAVEL",
+        metadata={"hnsw:space": "cosine"},
+        embedding_function=openai_ef
+    )
+    
+    return collection
     
 def generate_hw02(question, city, store_type, start_date, end_date):
     pass
@@ -36,3 +52,6 @@ def demo(question):
     )
     
     return collection
+
+
+#print(generate_hw01())
